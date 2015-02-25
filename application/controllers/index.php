cccb6e39c65e1e44dc56cda3b7da7dbdbf05fa92
Garbin <350496536@qq.com>
2015-02-25 15:02:04 +0800

    init CI configruation

---
+++ application/controllers/index.php
@@ -0,0 +1,14 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+	
+	class Index extends CI_Controller{
+		private $_data;
+		
+		function __construct(){
+			parent::__construct();
+		}
+		
+		function index(){
+			echo '123';
+		}
+	}
+?>
